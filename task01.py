cf13547802dab04b9e7951c72507928c03541298
javidaghayev <jawaghayev@gmail.com>
2024-03-12 10:53:38 +0400

    task01

---
+++ task01.py
@@ -0,0 +1,319 @@
+# # task1 
+
+
+
+# products = []
+
+
+# for i in range(0, 6):
+#     name = input("Name: ")
+#     price = float(input("Price: "))
+
+
+#     product = {
+#         "Name": name,
+#         "Price": price
+#     }
+
+#     products.append(product)
+
+# print("Products: ", products)
+
+
+
+
+
+# # task2 
+
+
+# sehmler = [] 
+
+
+# for i in range(0, 3):
+#     sehm_sayi = float(input("sehmlerin sayi: "))
+#     sehm_qiymeti = float(input("sehmlerin deyeri: "))
+#     sehm_deyeri = sehm_sayi * sehm_qiymeti
+
+#     sehm = {
+#         "Say": sehm_sayi,
+#         "Qiymet": sehm_qiymeti,
+#         "Deyer": sehm_deyeri
+#     }
+#     sehmler.append(sehm)
+
+# umumi_deyer = sehmler[0]["Deyer"] + sehmler[1]["Deyer"] + sehmler[2]["Deyer"]
+# print("Umumi deyer: ", umumi_deyer)
+
+
+
+
+
+
+
+
+
+
+
+# # task3
+
+
+# patients = []
+
+
+# for i in range(0, 4):
+#     name = input("name: ")
+#     age = int(input("age: "))
+
+
+#     patient = {
+#         "Name": name,
+#         "Age": age
+#     }
+
+#     patients.append(patient)
+
+# print("Patients: ", patients)
+
+
+
+
+# # task4 
+
+# students = []
+
+
+# for i in range(0, 8):
+#     name = input("Name: ")
+#     age = int(input("age: "))
+
+
+#     student = {
+#         "Name": name,
+#         "Age": age
+#     }
+
+#     students.append(student)
+
+# print("Students: ", students)
+
+ 
+ 
+
+# # task5 
+
+
+# products = []
+
+
+# for i in range(0, 4):
+#     name = input("Name: ")
+#     price = float(input("Price: "))
+
+
+#     product = {
+#         "Name": name,
+#         "Price": price
+#     }
+
+#     products.append(product)
+
+# print("Informations: ", products)
+ 
+
+
+
+
+
+# # task6 
+
+
+# informations = []
+
+
+# for i in range(0, 4):
+#     name = input("Name: ")
+#     age = int(input("Age: "))
+#     language = input("Programming language: ")
+
+
+#     information = {
+#         "Name": name,
+#         "Age": age,
+#         "Language": language
+#     }
+
+#     informations.append(information)
+
+# print("Informations: ", informations)
+
+
+
+
+
+
+# task7 
+
+
+# km = int(input("Qet edilen yol: "))
+# time = int(input("Serf edilen zaman (saat): "))
+# speed = km / time
+
+# print("Orta suret: ", int(speed))
+
+
+
+
+
+# # task8 
+
+
+
+# movies = []
+
+
+# for i in range(0, 4):
+#     name = input("Name: ")
+#     date = int(input("Date: "))
+
+
+#     film = {
+#         "Name": name,
+#         "Date": date
+#     }
+
+#     movies.append(film)
+
+# print("Films: ", movies)
+
+
+
+
+
+
+# # task9 
+
+
+
+# house = []
+
+
+# for i in range(0, 4):
+#     square = input("Evin kvadrat sahesi: ")
+#     price = input("Evin qiymeti: ")
+
+
+#     info = {
+#         "Square_meter": square,
+#         "Price": price
+#     }
+
+#     house.append(info)
+
+# print("Informations: ", house)
+
+
+
+
+# # task10 
+
+
+
+
+# products = []
+
+
+# for i in range(0, 7):
+#     name = input("Name: ")
+#     price = float(input("Price: "))
+
+
+#     product = {
+#         "Name": name,
+#         "Price": price
+#     }
+
+#     products.append(product)
+
+# print("Informations: ", products)
+
+
+
+# # task11 
+
+
+
+
+
+# products = []
+
+
+# for i in range(0, 2):
+#     name = input("Name: ")
+#     price = float(input("Price: "))
+
+
+#     product = {
+#         "Name": name,
+#         "Price": price
+#     }
+
+#     products.append(product)
+
+# print("Informations: ", products)
+
+
+
+
+
+# # task12 
+
+
+
+# phones = []
+
+
+# for i in range(0, 2):
+#     name = input("Phone name: ")
+#     price = float(input("Price: "))
+
+
+#     phone = {
+#         "Name": name,
+#         "Price": price
+#     }
+
+#     phones.append(phone)
+
+# print("Informations: ", phones)
+
+
+
+
+#  task13
+
+
+# ticket = float(input("Ticket price: "))
+# rentacar = float(input("Car price: "))
+# hotel = float(input("Hotel price: "))
+
+# total = ticket + rentacar + hotel
+
+# print("Total: ", total)
+
+
+
+
+
+
+
+# # task14
+
+
+# taxta = float(input("Taxtanin qiymeti: "))
+# sement = float(input("Sementin qiymeti: "))
+# qum = float(input("Qumun qiymeti: "))
+# demir = float(input("Demirin qiymeti: "))
+
+# toplam = taxta + sement + qum + demir
+# print("Toplam: ", toplam)
+ 
